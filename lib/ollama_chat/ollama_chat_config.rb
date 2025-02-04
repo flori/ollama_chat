e4ac940285ebb1ee5e6846787a083bc3695ee3fa
Florian Frank <flori@ping.de>
2025-02-04 21:09:12 +0100

    Simplified configuration defaults to be
    stored in default_config.yml
    
    - Replaced `DEFAULT_CONFIG` hash with a single line of code that reads
      from `default_config.yml`
    - Created new file `default_config.yml` in the same directory,
      containing the old `DEFAULT_CONFIG` hash values
    - Updated `initialize` method to use the new `default_config.yml` file
      if no filename is provided

--- lib/ollama_chat/ollama_chat_config.rb
+++ lib/ollama_chat/ollama_chat_config.rb
@@ -1,67 +1,12 @@
+require 'pathname'
+
 class OllamaChat::OllamaChatConfig
   include ComplexConfig
   include FileUtils
 
-  DEFAULT_CONFIG = <<~EOT
-      ---
-      url: <%= ENV['OLLAMA_URL'] || 'http://%s' % ENV.fetch('OLLAMA_HOST') %>
-      proxy: null # http://localhost:8080
-      model:
-        name: <%= ENV.fetch('OLLAMA_CHAT_MODEL', 'llama3.1') %>
-        options:
-          num_ctx: 8192
-      location:
-        enabled: false
-        name: Berlin
-        decimal_degrees: [ 52.514127, 13.475211 ]
-        units: SI (International System of Units) # or USCS (United States Customary System)
-      prompts:
-        embed: "This source was now embedded: %{source}"
-        summarize: |
-          Generate an abstract summary of the content in this document using
-          %{words} words:
-
-          %{source_content}
-        web: |
-          Answer the the query %{query} using these sources and summaries:
-
-          %{results}
-      system_prompts:
-        default: <%= ENV.fetch('OLLAMA_CHAT_SYSTEM', 'null') %>
-      voice:
-        enabled: false
-        default: Samantha
-        list: <%= `say -v ? 2>/dev/null`.lines.map { _1[/^(.+?)\s+[a-z]{2}_[a-zA-Z0-9]{2,}/, 1] }.uniq.sort.to_s.force_encoding('ASCII-8BIT') %>
-      markdown: true
-      stream: true
-      document_policy: importing
-      embedding:
-        enabled: true
-        model:
-          name: mxbai-embed-large
-          embedding_length: 1024
-          options: {}
-          # Retrieval prompt template:
-          prompt: 'Represent this sentence for searching relevant passages: %s'
-        batch_size: 10
-        database_filename: null # ':memory:'
-        collection: <%= ENV['OLLAMA_CHAT_COLLECTION'] %>
-        found_texts_size: 4096
-        found_texts_count: 10
-        splitter:
-          name: RecursiveCharacter
-          chunk_size: 1024
-      cache: Documentrix::Documents::SQLiteCache
-      redis:
-        documents:
-          url: <%= ENV.fetch('REDIS_URL', 'null') %>
-        expiring:
-          url: <%= ENV.fetch('REDIS_EXPIRING_URL', 'null') %>
-          ex: 86400
-      debug: <%= ENV['OLLAMA_CHAT_DEBUG'].to_i == 1 ? true : false %>
-      ssl_no_verify: []
-      copy: pbcopy
-  EOT
+  DEFAULT_CONFIG = File.read(
+    Pathname.new(__FILE__).dirname.join('ollama_chat_config/default_config.yml')
+  )
 
   def initialize(filename = nil)
     @filename = filename || default_path
