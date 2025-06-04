5f6eea80d265c7009b9edf15b81cdb552cf10ba4
Florian Frank <flori@ping.de>
2025-06-04 19:20:28 +0200

    Added method comments

--- lib/ollama_chat/clipboard.rb
+++ lib/ollama_chat/clipboard.rb
@@ -1,4 +1,13 @@
 module OllamaChat::Clipboard
+
+  # Copy the last assistant's message to the system clipboard.
+  #
+  # This method checks if there is a last message from an assistant in the `@messages`
+  # array and copies its content to the clipboard using the specified command from `config.copy`.
+  # If no assistant response is available or the clipboard command is not found, appropriate
+  # error messages are displayed.
+  #
+  # @return [NilClass] Always returns nil.
   def copy_to_clipboard
     if message = @messages.last and message.role == 'assistant'
       copy = `which #{config.copy}`.chomp
@@ -16,6 +25,13 @@ module OllamaChat::Clipboard
     nil
   end
 
+  # Paste content from the input.
+  #
+  # Prompts the user to paste their content and then press C-d (Ctrl+D) to terminate
+  # input. Reads all lines from standard input until Ctrl+D is pressed and returns
+  # the pasted content as a string.
+  #
+  # @return [String] The pasted content entered by the user.
   def paste_from_input
     STDOUT.puts bold { "Paste your content and then press C-d!" }
     STDIN.read
