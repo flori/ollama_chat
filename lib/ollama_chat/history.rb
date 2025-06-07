2c0546cbf0a3ad666b53c44377431b1e8867d7fd
Florian Frank <flori@ping.de>
2025-06-07 17:59:00 +0200

    Add OllamaChat::History comments

--- lib/ollama_chat/history.rb
+++ lib/ollama_chat/history.rb
@@ -1,8 +1,12 @@
 module OllamaChat::History
+  # Returns the full path of the chat history filename based on the
+  # configuration.
   def chat_history_filename
     File.expand_path(config.chat_history_filename)
   end
 
+  # Initializes the chat history by loading it from a file if it exists, and
+  # then loads the history into Readline::HISTORY.
   def init_chat_history
     if File.exist?(chat_history_filename)
       File.open(chat_history_filename, ?r) do |history|
@@ -13,10 +17,12 @@ module OllamaChat::History
     end
   end
 
+  # Saves the current chat history to a file in JSON format.
   def save_history
     File.secure_write(chat_history_filename, JSON.dump(Readline::HISTORY))
   end
 
+  # Clears all entries from Readline::HISTORY.
   def clear_history
     Readline::HISTORY.clear
   end
