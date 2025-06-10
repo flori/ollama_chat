efe8427628a83e04f49265a8e15cf3758f4053df
Florian Frank <flori@ping.de>
2025-06-11 01:36:38 +0200

    Documented Model handling methods in
    OllamaChat::ModelHandling

--- lib/ollama_chat/model_handling.rb
+++ lib/ollama_chat/model_handling.rb
@@ -1,15 +1,44 @@
 module OllamaChat::ModelHandling
+
+  # The model_present? method checks if the specified Ollama model is available.
+  #
+  # @param model [ String ] the name of the Ollama model
+  #
+  # @return [ String, FalseClass ] the system prompt if the model is present,
+  # false otherwise
   def model_present?(model)
     ollama.show(model:) { return _1.system.to_s }
   rescue Ollama::Errors::NotFoundError
     false
   end
 
+  # The pull_model_from_remote method attempts to retrieve a model from the
+  # remote server if it is not found locally.
+  #
+  # @param model [ String ] the name of the model to be pulled
+  #
+  # @return [ nil ]
   def pull_model_from_remote(model)
     STDOUT.puts "Model #{bold{model}} not found locally, attempting to pull it from remote now…"
     ollama.pull(model:)
   end
 
+  # The pull_model_unless_present method checks if the specified model is
+  # present on the system.
+  #
+  # If the model is already present, it returns the system prompt if it is
+  # present.
+  #
+  # Otherwise, it attempts to pull the model from the remote server using the
+  # pull_model_from_remote method. If the model is still not found after
+  # pulling, it exits the program with a message indicating that the model was
+  # not found remotely.
+  #
+  # @param model [ String ] The name of the model to check for presence.
+  # @param options [ Hash ] Options for the pull_model_from_remote method.
+  #
+  # @return [ String, FalseClass ] the system prompt if the model and it are
+  # present, false otherwise.
   def pull_model_unless_present(model, options)
     if system = model_present?(model)
       return system.full?
