f6b1f3e3ccb032396a8e9eef563250ba75f23390
Florian Frank <flori@ping.de>
2025-07-31 14:08:01 +0200

    Bump version to **0.0.18**
    - Updated `VERSION` in `VERSION` file from **0.0.17** to **0.0.18**
    - Updated `VERSION` constant in `lib/ollama_chat/version.rb` from **0.0.17** to **0.0.18**
    - Updated `s.version` in `ollama_chat.gemspec` from **0.0.17** to **0.0.18**

--- lib/ollama_chat/version.rb
+++ lib/ollama_chat/version.rb
@@ -1,6 +1,6 @@
 module OllamaChat
   # OllamaChat version
-  VERSION         = '0.0.17'
+  VERSION         = '0.0.18'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
