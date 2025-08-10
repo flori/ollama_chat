0e797b4e142b191c31b8ecd2e5c942f6a2c131b4
Florian Frank <flori@ping.de>
2025-08-11 00:21:07 +0200

    Update version to **0.0.19** across all files
    - Updated `VERSION` file to reflect new version **0.0.19**
    - Modified `lib/ollama_chat/version.rb` to set `VERSION` constant to **0.0.19**
    - Updated `ollama_chat.gemspec` version from **0.0.18** to **0.0.19**
    - Added `lib/ollama_chat/message_output.rb` to `s.extra_rdoc_files`
    - Added `lib/ollama_chat/message_output.rb` to `s.files`
    - Added `spec/ollama_chat/message_output_spec.rb` to `s.test_files`

--- lib/ollama_chat/version.rb
+++ lib/ollama_chat/version.rb
@@ -1,6 +1,6 @@
 module OllamaChat
   # OllamaChat version
-  VERSION         = '0.0.18'
+  VERSION         = '0.0.19'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
