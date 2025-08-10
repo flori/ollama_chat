abf676a0b2cd5650bc9c52a9f1e8fd205cf47f42
Florian Frank <flori@ping.de>
2025-08-11 01:06:49 +0200

    Add documentation and error handling to cache configuration method

--- lib/ollama_chat/document_cache.rb
+++ lib/ollama_chat/document_cache.rb
@@ -1,8 +1,20 @@
 module OllamaChat::DocumentCache
+  # The document_cache_class method returns the cache class specified in the
+  # configuration.
+  #
+  # @return [ Class ] the cache class defined by the config.cache setting
   def document_cache_class
     Object.const_get(config.cache)
   end
 
+  # The configure_cache method determines the appropriate cache class to use
+  # for document storage.
+  # It checks if the -M option was specified to use MemoryCache, otherwise it
+  # attempts to use the configured cache class.
+  # If an error occurs during this process, it falls back to using MemoryCache
+  # and reports the error.
+  #
+  # @return [ Class ] the selected cache class to be used for document caching
   def configure_cache
     if @opts[?M]
       Documentrix::Documents::MemoryCache
