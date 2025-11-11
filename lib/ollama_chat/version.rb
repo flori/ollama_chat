2baaae286efa2e3bf814f8c40dbf051362436f89
Florian Frank <flori@ping.de>
2025-11-11 19:44:33 +0100

    Update version to **0.0.38**
    - Updated `VERSION` file to **0.0.38**
    - Updated `OllamaChat::VERSION` constant to **0.0.38**
    - Updated gemspec version to **0.0.38**

--- lib/ollama_chat/version.rb
+++ lib/ollama_chat/version.rb
@@ -1,6 +1,6 @@
 module OllamaChat
   # OllamaChat version
-  VERSION         = '0.0.37'
+  VERSION         = '0.0.38'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
