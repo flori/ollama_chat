d8a6d41ad640d84c768747c68d1d3eabdb3e2cdb
Florian Frank <flori@ping.de>
2026-09-04 23:45:33 +0200

    Bump ollama_chat version to 0.0.118
    - Incremented gem version from **0.0.117** to **0.0.118** in `VERSION`,
      `lib/ollama_chat/version.rb`, and `ollama_chat.gemspec`.

--- lib/ollama_chat/version.rb
+++ lib/ollama_chat/version.rb
@@ -1,6 +1,6 @@
 module OllamaChat
   # OllamaChat version
-  VERSION         = '0.0.117'
+  VERSION         = '0.0.118'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
